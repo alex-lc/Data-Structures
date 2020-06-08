45e8ce0f5fa0c2d5ead15cdea6722a45a4de979d
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-06-08 16:49:29 -0500

    queue complete

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,5 @@
+from singly_linked_list import LinkedList
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -14,18 +16,64 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 
+# queues using list / array
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __str__(self):
+#         return (f'{self.storage}')
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if len(self.storage) > 0:
+#             return self.storage.pop(0)
+#         else:
+#             return None
+
+# checking tests
+# q = Queue()
+# q.enqueue(100)
+# q.enqueue(101)
+# q.enqueue(105)
+
+# print(q.dequeue())
+# print(len(q))
+
+# print(q.dequeue())
+# print(len(q))
+
+# print(q.dequeue())
+# print(len(q))
+
+# queues using LinkedList
+
 
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
+
+    def __str__(self):
+        return (f'{self.storage}')
 
     def __len__(self):
-        return len(self.storage)
+        return self.size
 
     def enqueue(self, value):
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        if len(self.storage) > 0:
-            return self.storage.pop()
+        if self.size > 0:
+            value = self.storage.remove_head()
+            self.size -= 1
+            return value
+        else:
+            return None
